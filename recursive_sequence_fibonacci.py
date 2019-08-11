a9b07dffa5ced952a02dcf62708fe1ef6dd1729a
potatoman16 <virani.imran@icloud.com>
2019-08-11 17:00:16 -0400

    Logic

--- recursive_sequence_fibonacci.py
+++ recursive_sequence_fibonacci.py
@@ -4,3 +4,12 @@ t1 = 1
 tn = 1
 tbefore = 0
 counter = 1
+if N == 0:
+    print()
+else:
+    print(t1)
+    while counter < N:
+        print(tn)
+        tbefore = tn - tbefore
+        tn = tbefore + tn
+        counter += 1
