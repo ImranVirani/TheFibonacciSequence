e330cde565a111f02c94a5eaad0e49c408d0f909
potatoman16 <virani.imran@icloud.com>
2019-08-11 16:59:27 -0400

    Initialize Variables needed

--- recursive_sequence_fibonacci.py
+++ recursive_sequence_fibonacci.py
@@ -1,3 +1,6 @@
 # This program generates the fibonacci sequence to the Nth number
 N = int(input("How many terms of the fibonacci sequence should be generated?"))
-
+t1 = 1
+tn = 1
+tbefore = 0
+counter = 1
