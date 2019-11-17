0167aba48bbd560626a3a9cd76a432d753094e41
potatoman16 <32938830+potatoman16@users.noreply.github.com>
2019-11-17 16:27:28 -0500

    Update recursive_sequence_fibonacci.py

--- recursive_sequence_fibonacci.py
+++ recursive_sequence_fibonacci.py
@@ -4,12 +4,13 @@ t1 = 1
 tn = 1
 tbefore = 0
 counter = 1
-if N == 0:
-    print()
-else:
-    print(t1)
-    while counter < N:
-        print(tn)
-        tbefore = tn - tbefore
-        tn = tbefore + tn
-        counter += 1
+while N == 0:
+    print("You cannot display 0 terms of the fibonacci sequence")
+    N = int(input("How many terms of the fibonacci sequence should be generated?"))
+
+print(t1)
+while counter < N:
+    print(tn)
+    tbefore = tn - tbefore
+    tn = tbefore + tn
+    counter += 1
